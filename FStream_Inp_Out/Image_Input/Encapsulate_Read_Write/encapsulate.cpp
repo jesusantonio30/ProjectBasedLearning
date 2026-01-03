c304a63240b2ba8821b0921113f63a44c3574256
Jesus Morales <morales.jesusant@gmail.com>
2026-01-02 23:47:59 -0800

    Modularized my program for reusability

---
+++ FStream_Inp_Out/Image_Input/Encapsulate_Read_Write/encapsulate.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <iterator>
+#include <string>
+#include <stdexcept>
+
+std::vector<char> readBinaryFile(const std::string& binary_path);
+bool writeBinaryFile(const std::string& binary_path, const std::vector<char>& raw_data);
+
+int main() {
+
+    std::string binary_file = "ex_img.webp";
+    std::string copy_binary = "copy_" + binary_file;
+
+    std::vector<char> data = readBinaryFile(binary_file);
+
+    writeBinaryFile(copy_binary, data);
+
+    return 0;
+}
+
+
+std::vector<char> readBinaryFile(const std::string& binary_path) {
+    std::ifstream my_file(binary_path, std::ios::binary);
+
+    if (!my_file) {
+        throw std::runtime_error("Error: Unable to open file '" + binary_path + "'.");
+    }
+
+    my_file.exceptions(std::ios::badbit);
+
+    std::istreambuf_iterator<char> begin(my_file);
+    std::istreambuf_iterator<char> end;
+
+    return std::vector<char>(begin, end);
+}
+
+bool writeBinaryFile(const std::string& binary_path, const std::vector<char>& raw_data) {
+    std::ofstream new_file(binary_path, std::ios::binary);
+
+    if (!new_file) {
+        throw std::runtime_error("Error: Unable to write to file '" + binary_path + "'.");
+    }
+
+    new_file.exceptions(std::ios::badbit);
+
+    new_file.write(raw_data.data(), raw_data.size());
+
+    new_file.flush();
+
+    return new_file.good();
+}
+
+
