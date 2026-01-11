1827f7df5cbd097359d6ef4061bafc04e7552d32
Jesus Morales <morales.jesusant@gmail.com>
2026-01-11 11:10:11 -0800

    Created a method to assign tasks to each thread.

--- Concurrency/threads_intro/Threads_Practice/Assignment1/Parallel_File_Hasher.cpp
+++ Concurrency/threads_intro/Threads_Practice/Assignment1/Parallel_File_Hasher.cpp
@@ -21,25 +21,47 @@ namespace filesys = std::filesystem;
     Main thread waits for all threads → merges all results → prints sorted by filename
 */
 
-// Iterate over test files and store file names
-
 int main() {
+
+    // Iterate over test files and store file names
 
     std::string path = "./test_files/";
 
-    std::vector<std::string> fileNames;
+    std::vector<std::string> files;
 
     for (filesys::directory_iterator iter(path); iter != filesys::directory_iterator(); ++iter) {
         try {
             if (iter->is_regular_file()) {
-                fileNames.push_back(iter->path().filename().string());
+                files.push_back(iter->path().filename().string());
             }
-            
+
         } catch (filesys::filesystem_error& err) {
             std::cerr << err.what() << std::endl;
             return 1;
         }
     }
+
+    // Declare number of threads
+    constexpr size_t NUM_THREADS = 8;
+
+    // Reserve spots for each thread and it's duties
+    std::vector<std::vector<std::string>> threadDuties(NUM_THREADS);
+
+    // Determine how many tasks to assign
+    size_t numTasks = (files.size() + NUM_THREADS - 1) / NUM_THREADS;
+
+    // Iterate through vector of threads
+    for (size_t thread = 0; thread < NUM_THREADS; ++thread) {
+
+        size_t startFile = thread * numTasks;
+        size_t endFile = std::min(startFile + numTasks, files.size());
+
+        // Assign files to each thread
+        for (size_t i = startFile; i < endFile; ++i) {
+            threadDuties[thread].push_back(files[i]);
+        }
+    }
+
 
     return 0;
 }
